58d95639b5ae513045fec71c96b693154b15ebd4
Vincent Chang <vntchang@gmail.com>
2024-09-26 22:16:38 +0800

    day 12 panning clamping functionality

--- src/camera.ts
+++ src/camera.ts
@@ -1,16 +1,29 @@
 import { Point } from "vector";
-import { multiplyByScalar, rotateVector, vectorAddition, vectorSubtraction } from "./vector";
+import { multiplyByScalar, rotateVector, vectorAddition, vectorSubtraction, getLineSegmentIntersection } from "./vector";
 
-
+export type PositionBoundary = {
+    min: Point;
+    max: Point;
+}
 class Camera {
     private _position: Point;
     private _zoomLevel: number;
     private _rotation: number;
+    private _positionBoundary: PositionBoundary;
 
-    constructor(){
+    public viewPortWidth: number;
+    public viewPortHeight: number;
+
+    public limitEntireViewPort: boolean;
+
+    constructor(viewPortWidth: number = 500, viewPortHeight: number = 500, positionBoundary: PositionBoundary = {min: {x: -1000, y: -1000}, max: {x: 1000, y: 1000}}){
         this._position = {x: 0, y: 0};
         this._zoomLevel = 1; // 縮放程度不能夠小於或是等於 0。
         this._rotation = 0;
+        this._positionBoundary = positionBoundary;
+        this.viewPortHeight = viewPortHeight;
+        this.viewPortWidth = viewPortWidth;
+        this.limitEntireViewPort = false;
     }
 
     get position(): Point {
@@ -26,12 +39,23 @@ class Camera {
     }
 
     setPosition(destination: Point){
+        if(this.limitEntireViewPort && !viewPortWithinPositionBoundary(this.viewPortWidth, this.viewPortHeight, destination, this._zoomLevel, this._rotation, this._positionBoundary)){
+            return;
+        }
+        if(!withinPositionBoundary(destination, this._positionBoundary)){
+            return;
+        }
         this._position = destination;
     }
 
     setPositionBy(offset: Point){
         const destination = vectorAddition(this._position, offset);
-        this.setPosition(destination);
+        if(this.limitEntireViewPort){
+            this.setPosition(clampingEntireViewPort(this.viewPortWidth, this.viewPortHeight, destination, this._rotation, this._zoomLevel, this._positionBoundary));
+            return;
+        }
+        const clampedDestination = simpleClamping(destination, this._positionBoundary);
+        this.setPosition(clampedDestination);
     }
     
     setZoomLevel(targetZoom: number){
@@ -43,10 +67,7 @@ class Camera {
     }
 
     transformViewPort2WorldSpace(point: Point): Point {
-        const scaledBack = multiplyByScalar(point, 1 / this._zoomLevel);
-        const rotatedBack = rotateVector(scaledBack, this._rotation);
-        const withOffset = vectorAddition(rotatedBack, this._position);
-        return withOffset;
+        return transformViewPort2WorldSpaceWithCameraAttributes(point, this._position, this._zoomLevel, this._rotation);
     }
 
     transformWorldSpace2ViewPort(point: Point): Point {
@@ -59,6 +80,159 @@ class Camera {
     transformVector2WorldSpace(vector: Point): Point{
         return rotateVector(multiplyByScalar(vector, 1 / this._zoomLevel), this._rotation);
     }
+}
+
+function withinPositionBoundary(destination: Point, positionBoundary: PositionBoundary): boolean {
+    if(destination.x > positionBoundary.max.x || destination.x < positionBoundary.min.x){
+        return false;
+    }
+    if(destination.y > positionBoundary.max.y || destination.y < positionBoundary.min.y){
+        return false;
+    }
+    return true;
+}
+
+export function simpleClamping(destination: Point, positionBoundary: PositionBoundary): Point {
+    if(withinPositionBoundary(destination, positionBoundary)){
+        return destination;
+    }
+
+    const res = {...destination};
+
+    res.x = Math.min(res.x, positionBoundary.max.x);
+    res.x = Math.max(res.x, positionBoundary.min.x);
+
+    res.y = Math.min(res.y, positionBoundary.max.y);
+    res.y = Math.max(res.y, positionBoundary.min.y);
+
+    return res;
+}
+
+export function clampingV2(origin: Point, destination: Point, positionBoundary: PositionBoundary): Point {
+    if (withinPositionBoundary(destination, positionBoundary)){
+        return destination;
+    }
+
+    const topRight = {x: positionBoundary.max.x, y: positionBoundary.max.y};
+    const bottomRight = {x: positionBoundary.max.x, y: positionBoundary.min.y};
+    const topLeft = {x: positionBoundary.min.x, y: positionBoundary.max.y};
+    const bottomLeft = {x: positionBoundary.min.x, y: positionBoundary.min.y};
+
+    const surpassedTop = destination.y > topLeft.y;
+    const surpassedRight = destination.x > topRight.x;
+    const surpassedBottom = destination.y < bottomRight.y;
+    const surpassedLeft = destination.x < bottomLeft.x;
+
+    let manipulatePoint = {...destination};
+
+    if(surpassedTop && surpassedRight){
+        console.log("top right");
+        return topRight;
+    }
+    if(surpassedTop && surpassedLeft){
+        console.log("top left");
+        return topLeft;
+    }
+    if(surpassedBottom && surpassedRight){
+        console.log("bottom right");
+        return bottomRight;
+    }
+    if(surpassedBottom && surpassedLeft){
+        console.log("bottom left");
+        return bottomLeft;
+    }
+
+    let boundaryStart = bottomRight;
+    let boundaryEnd = topRight;
+    
+    if(surpassedTop){
+        boundaryStart = topLeft;
+        boundaryEnd = topRight;
+    } else if(surpassedBottom){
+        boundaryStart = bottomLeft;
+        boundaryEnd = bottomRight;
+    } else if(surpassedLeft){
+        boundaryStart = bottomLeft;
+        boundaryEnd = topLeft;
+    }
+    const res = getLineSegmentIntersection(origin, destination, boundaryStart, boundaryEnd);
+    if(!res.intersects){
+        throw new Error("should have intersection but cannot calculate one");
+    }
+    switch(res.intersections.intersectionType){
+    case "point":
+        manipulatePoint = {...res.intersections.intersectionPoint};
+        break;
+    case "interval":
+        manipulatePoint = {...res.intersections.intervalEndPoint};
+        break;
+    default:
+        throw new Error("with intersections but the type is unknown");
+    }
+    return manipulatePoint;
+}
+
+function transformViewPort2WorldSpaceWithCameraAttributes(point: Point, cameraPosition: Point, cameraZoomLevel: number, cameraRotation: number): Point{
+    const scaledBack = multiplyByScalar(point, 1 / cameraZoomLevel);
+    const rotatedBack = rotateVector(scaledBack, cameraRotation);
+    const withOffset = vectorAddition(rotatedBack, cameraPosition);
+    return withOffset;
+}
+
+function viewPortWithinPositionBoundary(viewPortWidth: number, viewPortHeight: number, cameraPosition: Point, cameraZoomLevel: number, cameraRotation: number, positionBoundary: PositionBoundary): boolean {
+    const topLeftCorner = {x: -viewPortWidth / 2, y: viewPortHeight / 2};
+    const topRightCorner = {x: viewPortWidth / 2, y: viewPortHeight / 2};
+    const bottomLeftCorner = {x: -viewPortWidth / 2, y: -viewPortHeight / 2};
+    const bottomRightCorner = {x: viewPortWidth / 2, y: -viewPortHeight / 2};
+    
+    const topLeftCornerTransformed = transformViewPort2WorldSpaceWithCameraAttributes(topLeftCorner, cameraPosition, cameraZoomLevel, cameraRotation);
+    const topRightCornerTransformed = transformViewPort2WorldSpaceWithCameraAttributes(topRightCorner, cameraPosition, cameraZoomLevel, cameraRotation);
+    const bottomLeftCornerTransformed = transformViewPort2WorldSpaceWithCameraAttributes(bottomLeftCorner, cameraPosition, cameraZoomLevel, cameraRotation);
+    const bottomRightCornerTransformed = transformViewPort2WorldSpaceWithCameraAttributes(bottomRightCorner, cameraPosition, cameraZoomLevel, cameraRotation);
+    
+    return withinPositionBoundary(topLeftCornerTransformed, positionBoundary) && withinPositionBoundary(topRightCornerTransformed, positionBoundary) && withinPositionBoundary(bottomLeftCornerTransformed, positionBoundary) && withinPositionBoundary(bottomRightCornerTransformed, positionBoundary);
+}
+
+function clampingEntireViewPort(viewPortWidth: number, viewPortHeight: number, targetCameraPosition: Point, cameraRotation: number, cameraZoomLevel: number, positionBoundary: PositionBoundary): Point {
+    const topLeftCorner = {x: -viewPortWidth / 2, y: viewPortHeight / 2};
+    const topRightCorner = {x: viewPortWidth / 2, y: viewPortHeight / 2};
+    const bottomLeftCorner = {x: -viewPortWidth / 2, y: -viewPortHeight / 2};
+    const bottomRightCorner = {x: viewPortWidth / 2, y: -viewPortHeight / 2};
+    
+    const topLeftCornerTransformed = transformViewPort2WorldSpaceWithCameraAttributes(topLeftCorner, targetCameraPosition, cameraZoomLevel, cameraRotation);
+    const topRightCornerTransformed = transformViewPort2WorldSpaceWithCameraAttributes(topRightCorner, targetCameraPosition, cameraZoomLevel, cameraRotation);
+    const bottomLeftCornerTransformed = transformViewPort2WorldSpaceWithCameraAttributes(bottomLeftCorner, targetCameraPosition, cameraZoomLevel, cameraRotation);
+    const bottomRightCornerTransformed = transformViewPort2WorldSpaceWithCameraAttributes(bottomRightCorner, targetCameraPosition, cameraZoomLevel, cameraRotation);
+    
+    if( withinPositionBoundary(topLeftCornerTransformed, positionBoundary) && withinPositionBoundary(topRightCornerTransformed, positionBoundary) && withinPositionBoundary(bottomLeftCornerTransformed, positionBoundary) && withinPositionBoundary(bottomRightCornerTransformed, positionBoundary)){
+        return targetCameraPosition;
+    }   
+    const topLeftCornerClamped = simpleClamping(topLeftCornerTransformed, positionBoundary);
+    const topRightCornerClamped = simpleClamping(topRightCornerTransformed, positionBoundary);
+    const bottomLeftCornerClamped = simpleClamping(bottomLeftCornerTransformed, positionBoundary);
+    const bottomRightCornerClamped = simpleClamping(bottomRightCornerTransformed, positionBoundary);
+
+    const topLeftCornerDelta = vectorSubtraction(topLeftCornerClamped, topLeftCornerTransformed);
+    const topRightCornerDelta = vectorSubtraction(topRightCornerClamped, topRightCornerTransformed);
+    const bottomLeftCornerDelta = vectorSubtraction(bottomLeftCornerClamped, bottomLeftCornerTransformed);
+    const bottomRightCornerDelta = vectorSubtraction(bottomRightCornerClamped, bottomRightCornerTransformed);
+    
+    let diffs = [topLeftCornerDelta, topRightCornerDelta, bottomLeftCornerDelta, bottomRightCornerDelta];
+    let maxXDiff = Math.abs(diffs[0].x);
+    let maxYDiff = Math.abs(diffs[0].y);
+    let delta = diffs[0];
+    diffs.forEach((diff)=>{
+        if(Math.abs(diff.x) > maxXDiff){
+            maxXDiff = Math.abs(diff.x);
+            delta.x = diff.x;
+        }
+        if(Math.abs(diff.y) > maxYDiff){
+            maxYDiff = Math.abs(diff.y);
+            delta.y = diff.y;
+        }
+    });
+
+    return vectorAddition(delta, targetCameraPosition);
 }
 
 export { Camera };
--- src/vector.ts
+++ src/vector.ts
@@ -35,3 +35,259 @@ export function rotateVector(vector: Point, angle: number): Point {
 export function angleFromA2B(vectorA: Point, vectorB: Point): number {
     return Math.atan2(vectorA.x * vectorB.y - vectorA.y * vectorB.x, vectorA.x * vectorB.x + vectorA.y * vectorB.y);
 }
+
+export type PointIntersection = {
+    intersectionType: "point";
+    intersectionPoint: Point;
+    ratio: number;
+}
+
+export type IntervalIntersection = {
+    intersectionType: "interval";
+    intervalStartPoint: Point;
+    intervalEndPoint: Point;
+    startRatio: number;
+    endRatio: number;
+}
+
+export type IntersectionPositive = {
+    intersects: true;
+    intersections: IntervalIntersection | PointIntersection;
+}
+
+export type IntersectionNegative = {
+    intersects: false;
+}
+
+export type IntersectionResult = IntersectionPositive | IntersectionNegative;
+
+export function pointLiesOnSegment(point: Point, startPoint: Point, endPoint: Point): boolean {
+    const areaCheck = startPoint.x * (endPoint.y - point.y) + endPoint.x * (point.y - startPoint.y) + point.x * (startPoint.y - endPoint.y);
+    if(areaCheck !== 0) {
+        return false;
+    }
+    let xInRange = Math.min(endPoint.x, startPoint.x) <= point.x;
+    xInRange = Math.max(endPoint.x, startPoint.x) >= point.x;
+    let yInRange = Math.min(endPoint.y, startPoint.y) <= point.y;
+    yInRange = Math.max(endPoint.y, startPoint.y) >= point.y;
+
+    return xInRange && yInRange;
+}
+
+export function intervalsOverlap(interval1: {start: number, end: number}, interval2: {start: number, end: number}): boolean {
+    const transformedInterval1 = interval1.start > interval1.end ? {start: interval1.end, end: interval1.start} : interval1;
+    const transformedInterval2 = interval2.start > interval2.end ? {start: interval2.end, end: interval2.start} : interval2;
+    return transformedInterval1.start <= transformedInterval2.end && transformedInterval2.start <= transformedInterval1.end;
+}
+
+export function pointsAreEqual(point: Point, point2: Point): boolean {
+    return point.x === point2.x && point.y === point2.y;
+}
+
+export function slopeOf(startPoint: Point, endPoint: Point): number {
+    return (endPoint.y - startPoint.y) / (endPoint.x - startPoint.x);
+}
+
+export function offsetOf(startPoint: Point, endPoint: Point): number {
+    return startPoint.y - slopeOf(startPoint, endPoint) * startPoint.x;
+}
+
+export type PositiveOverlapInterval = {
+    overlaps: true;
+    start: number;
+    end: number;
+}
+
+export type NegativeOverlapInterval = {
+    overlaps: false;
+}
+
+export type OverlapIntervalResult = PositiveOverlapInterval | NegativeOverlapInterval;
+
+export function getOverlapStartAndEnd(interval1: {start: number, end: number}, interval2: {start: number, end: number}): OverlapIntervalResult{
+    const transformedInterval1 = interval1.start > interval1.end ? {start: interval1.end, end: interval1.start} : interval1;
+    const transformedInterval2 = interval2.start > interval2.end ? {start: interval2.end, end: interval2.start} : interval2;
+    if(!intervalsOverlap(transformedInterval1, transformedInterval2)) {
+        return {
+            overlaps: false,
+        }
+    }
+    return {overlaps: true, start: Math.max(transformedInterval1.start, transformedInterval2.start), end: Math.min(transformedInterval1.end, transformedInterval2.end)};
+}
+
+export function linearInterpolation(startPoint: Point, endPoint: Point, ratio: number): Point {
+    return {x: startPoint.x + (endPoint.x - startPoint.x) * ratio, y: startPoint.y + (endPoint.y - startPoint.y) * ratio};
+}
+
+export function getLineSegmentIntersection(startPoint: Point, endPoint: Point, startPoint2: Point, endPoint2: Point): IntersectionResult {
+
+    if(pointsAreEqual(startPoint, endPoint) && pointsAreEqual(startPoint2, endPoint2)){
+        // 兩個線段皆為點，我們只需要檢查兩點是不是一樣的即可
+        if(!pointsAreEqual(startPoint, startPoint2)) {
+            return {
+                intersects: false
+            };
+        }
+        return {
+            intersects: true,
+            intersections: {
+                intersectionType: "point",
+                intersectionPoint: startPoint,
+                ratio: 0,
+            }
+        }
+    }
+
+    if(pointsAreEqual(startPoint, endPoint)){
+        // 第一個線段是個點
+        if(!pointLiesOnSegment(startPoint, startPoint2, endPoint2)){
+            return {
+                intersects: false,
+            };
+        }
+        return {
+            intersects: true,
+            intersections: {
+                intersectionType: "point",
+                intersectionPoint: startPoint,
+                ratio: 0,
+            }
+        }
+    }
+
+    if(pointsAreEqual(startPoint2, endPoint2)){
+        // 第二個線段是個點
+        if(!pointLiesOnSegment(startPoint2, startPoint, endPoint)){
+            return {
+                intersects: false,
+            };
+        }
+        return {
+            intersects: true,
+            intersections: {
+                intersectionType: "point",
+                intersectionPoint: startPoint2,
+                ratio: (startPoint2.x - startPoint.x) / (endPoint.x - startPoint.x),
+            }
+        }
+    }
+
+    // complete overlap
+    if((pointsAreEqual(startPoint, startPoint2) && pointsAreEqual(endPoint, endPoint2)) || (pointsAreEqual(startPoint, endPoint2) && pointsAreEqual(endPoint, startPoint2))){
+        return { 
+            intersects: true, 
+            intersections: {
+                intersectionType: "interval",
+                intervalStartPoint: startPoint,
+                intervalEndPoint: endPoint,
+                startRatio: 0,
+                endRatio: 1,
+            }
+        }
+    }
+
+    // point overlap
+    if(pointsAreEqual(startPoint, startPoint2)){
+        return {
+            intersects: true,
+            intersections: {
+                intersectionType: "point",
+                intersectionPoint: startPoint,
+                ratio: 0,
+            }
+        }
+    }
+
+    if(pointsAreEqual(startPoint, endPoint2)){
+        return {
+            intersects: true,
+            intersections: {
+                intersectionType: "point",
+                intersectionPoint: startPoint,
+                ratio: 0,
+            }
+        }
+    }
+
+    if(pointsAreEqual(endPoint, startPoint2)){
+        return {
+            intersects: true,
+            intersections: {
+                intersectionType: "point",
+                intersectionPoint: endPoint,
+                ratio: 1,
+            }
+        }
+    }
+
+    if(pointsAreEqual(endPoint, endPoint2)){
+        return {
+            intersects: true,
+            intersections: {
+                intersectionType: "point",
+                intersectionPoint: endPoint,
+                ratio: 1,
+            }
+        }
+    }
+    const numerator = (endPoint2.x - startPoint2.x) * (startPoint.y - startPoint2.y) - (endPoint2.y - startPoint2.y) * (startPoint.x - startPoint2.x);
+    const denominator = (endPoint2.y - startPoint2.y) * (endPoint.x - startPoint.x) - (endPoint2.x - startPoint2.x) * (endPoint.y - startPoint.y);
+
+    if(denominator == 0 && ((endPoint.x !== startPoint.x && offsetOf(startPoint, endPoint) !== offsetOf(startPoint2, endPoint2)) || (endPoint.x === startPoint.x && endPoint.x !== endPoint2.x))){
+        return { intersects: false };
+    }
+
+    if(denominator == 0){
+        const unit = unitVector(vectorSubtraction(endPoint, startPoint));
+
+        const start = dotProduct(unit, startPoint);
+        const end = dotProduct(unit, endPoint);
+
+        const start2 = dotProduct(unit, startPoint2);
+        const end2 = dotProduct(unit, endPoint2);
+
+        const res = getOverlapStartAndEnd({start, end}, {start: start2, end: end2});
+        if(res.overlaps){
+            const intervalStartPoint = multiplyByScalar(unit, res.start);
+            const intervalEndPoint = multiplyByScalar(unit, res.end);
+            const startRatio = (res.start - start) / (end - start);
+            const endRatio = (res.end - start) / (end - start);
+            return start > end ? {
+                intersects: true, 
+                intersections: {
+                    intersectionType: "interval",
+                    intervalStartPoint: intervalEndPoint,
+                    intervalEndPoint: intervalStartPoint,
+                    startRatio: endRatio,
+                    endRatio: startRatio,
+                }
+            } : {
+                intersects: true, 
+                intersections: {
+                    intersectionType: "interval",
+                    intervalStartPoint: intervalStartPoint,
+                    intervalEndPoint: intervalEndPoint,
+                    startRatio: startRatio,
+                    endRatio: endRatio,
+                }
+            };
+        } else {
+            return { intersects: false };
+        }
+    }
+    const ratio = numerator / denominator;
+    if (ratio >= 0 && ratio <= 1){
+        return {
+            intersects: true,
+            intersections: {
+                intersectionType: "point",
+                intersectionPoint: linearInterpolation(startPoint, endPoint, ratio),
+                ratio: ratio, 
+            }
+        }
+    }
+
+    return {
+        intersects: false,
+    }
+}
